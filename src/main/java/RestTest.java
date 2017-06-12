5c7db48858293c60a7479e9fa417a55665634bf9
k <laesunk@gmail.com>
2017-06-13 00:46:21 +0900

    rest api setup

---
+++ src/main/java/RestTest.java
@@ -0,0 +1,22 @@
+/**
+ * Created by K on 2017. 6. 13..
+ */
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class RestTest {
+
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+}
